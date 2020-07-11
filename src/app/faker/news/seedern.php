23cd8541e534f5e70ca3c580d704ff0646e02f9c
Andre Amorim <andreamorimsimoes@gmail.com>
2020-07-11 17:25:16 +0100

    faker creates dir with section name in uploads

--- src/app/faker/news/seedern.php
+++ src/app/faker/news/seedern.php
@@ -12,19 +12,21 @@ function seedernews($input)
     $sql = "SET GLOBAL FOREIGN_KEY_CHECKS = 1";
     $stmt = conn()->prepare($sql);
     $stmt->execute();
+    deleteDirectory ($_SERVER['DOCUMENT_ROOT']."/app/uploads/news");
     
     for ($i = 0; $i < $input; $i++) {
       $title      = $faker->sentence($nbWords = 3, $variableNbWords = true);
       $summary    = $faker->sentence($nbWords = 6, $variableNbWords = true);
-      $body       = $faker->text($maxNbChars = 400);
+      $body       = $faker->text($maxNbChars = 1000);
       $date       = $faker->date;
       $author     = $faker->name;
       $token      = $faker->uuid;
       $status     = $faker->numberBetween($min = 0, $max = 2);
       $users_id   = 1; // RANDOM USER INSERT PRODUCTS OR NEWS
-      mkdir($_SERVER['DOCUMENT_ROOT']."/app/uploads/$token/",0777);
-      $image      = $faker->image($_SERVER['DOCUMENT_ROOT']."/app/uploads/$token", 800, 600, null, false);
-      rename ($_SERVER['DOCUMENT_ROOT']."/app/uploads/$token/".$image, $_SERVER['DOCUMENT_ROOT']."/app/uploads/$token/1.jpg");
+      
+      mkdir($_SERVER['DOCUMENT_ROOT']."/app/uploads/news/$token/",0777,true);
+      $image      = $faker->image($_SERVER['DOCUMENT_ROOT']."/app/uploads/news/$token", 800, 600, null, false);
+      rename ($_SERVER['DOCUMENT_ROOT']."/app/uploads/news/$token/".$image, $_SERVER['DOCUMENT_ROOT']."/app/uploads/news/$token/1.jpg");
 
       $sql = "INSERT INTO news (title, summary, body, date, author, token, status, users_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
       $stmt = conn()->prepare($sql);
