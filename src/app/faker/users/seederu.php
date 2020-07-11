882e00c58acda8617e64140ea409b72170721d1d
Andre Amorim <andreamorimsimoes@gmail.com>
2020-07-11 17:27:51 +0100

    insert a 2nd user to run some internal tests

--- src/app/faker/users/seederu.php
+++ src/app/faker/users/seederu.php
@@ -12,13 +12,26 @@ function seederusers($input)
     $sql = "SET GLOBAL FOREIGN_KEY_CHECKS = 1";
     $stmt = conn()->prepare($sql);
     $stmt->execute();
+     // 1nd mandatory INSERT (database test)
     $name = 'Amorim+Admin';
-    $email = 'andreamorimsimoes@gmail.com';
+    $email = 'amorimadmin@company.com';
     $password = password_hash("123",PASSWORD_BCRYPT);
     $status = 1;
     $token = $faker->uuid;
     $level = 2;
     deleteDirectory('../uploads/avatar');
+    
+    $sql = "INSERT INTO users (name, email, password, status, token, level) VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = conn()->prepare($sql);
+    $stmt->execute([$name, $email, $password, $status, $token, $level]);
+
+    // 2nd mandatory INSERT (database test)
+    $name = 'Amorim+User';
+    $email = 'amorimuser@company.com';
+    $password = password_hash("123",PASSWORD_BCRYPT);
+    $status = 1;
+    $token = $faker->uuid;
+    $level = 1;
     
     $sql = "INSERT INTO users (name, email, password, status, token, level) VALUES (?, ?, ?, ?, ?, ?)";
     $stmt = conn()->prepare($sql);
