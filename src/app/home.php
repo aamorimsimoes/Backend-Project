4e89d55f473c7e16fb33e22b847788e80ac29b93
Andre Amorim <andreamorimsimoes@gmail.com>
2020-07-13 02:09:43 +0100

    Returns Unix timestamp representing the date (dev)

--- src/app/home.php
+++ src/app/home.php
@@ -4,7 +4,10 @@ if (!isset($_SESSION['loggedin'])) {
   header('Location: signin.php');
   exit;
 } else {
-  require_once('includes/head.php'); ?>
+  require_once('includes/head.php');
+  $now = new DateTime();
+  $timestamp = $now->getTimestamp();
+?>
 
   <body class="app">
     <?php
@@ -24,16 +27,17 @@ if (!isset($_SESSION['loggedin'])) {
               echo "<h1 style='width:100%;display:block;text-align:center'>Latest News:</h1>";
               $data = $stmt->fetchAll();
               $stmt = null;
+
               foreach ($data as $r) { ?>
                 <div class="card" onclick="location.href='crud/read.php?s=news&token=<?= $r['token'] ?>'" style="cursor: pointer;">
-                  <img src="uploads/news/<?= $r['token'] ?>/1.jpg" alt="newsImage" style="width:100%">
+                  <img src="uploads/news/<?= $r['token'] ?>/1.jpg?t=<?= $timestamp ?>" alt="newsImage" style="width:100%">
                   <div class="card-container">
                     <p style="font-size:smaller;display:flex;align-items:center">by <?= $r['author'] ?> on <?= $r['date'] ?></p>
                     <h4><b><?= $r['title'] ?></b></h4>
                     <p><?= $r['summary'] ?></p>
                   </div>
                 </div>
-          <?php
+              <?php
               }
             }
           }
